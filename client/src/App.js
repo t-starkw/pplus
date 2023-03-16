72556ea96a4611ff36f327ca8e658ec0ff72c309
Tessa <tess.starkweather@gmail.com>
2023-03-16 14:15:53 -0400

    added code for pages and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Nav from './components/Navbar'
-
+import Landing from './pages/Landing';
+import GeneratorPage from './pages/Generator';
 import './styles/App.css';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
     <>
       <Nav />
-
+      <GeneratorPage />
     </>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -28,14 +28,13 @@ function Nav() {
                             <li>
                                 <a href="#"
                                     className="block py-2 pl-3 pr-4 text-gray rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-slate-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray dark:hover:text-slate-900 md:dark:hover:bg-transparent">
-                                    Log In</a>
+                                    Profile</a>
                             </li>
                             <li>
                                 <a href="#"
                                     className="block py-2 pl-3 pr-4 text-gray rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-slate-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray dark:hover:text-slate-900 md:dark:hover:bg-transparent">
-                                    Sign Up</a>
+                                    Log In/Signup</a>
                             </li>
-
                         </ul>
                     </div>
                 </div>
