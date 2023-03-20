3fdffe7e732af7e58c428bac346b1ba85a77842d
Tessa <tess.starkweather@gmail.com>
2023-03-20 17:58:51 -0400

    add apollo client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+{
+  me {
+    _id
+    email
+    name
+    pwdCount
+    savedPasswords {
+      password
+      site
+      _id
+    }
+  }
+}
+`;
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv'). config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://tstarkw:JXbbQ8S8mSzRjle3@tessacluster.hocfryr.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://tstarkw:JXbbQ8S8mSzRjle3@tessacluster.hocfryr.mongodb.net/pplus", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
